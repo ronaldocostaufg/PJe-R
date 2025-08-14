7bed052c28df9bf307b4476ee2ef606ef9339892
Vinícius Benevides <136761013+ViniciusBenevides@users.noreply.github.com>
2025-08-14 11:47:05 -0300

    Update urls.py

--- folhafacil/urls.py
+++ folhafacil/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for folhafacil project. TESTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+URL configuration for folhafacil project. uguigiugdiudwgidwgdwigwiug
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
