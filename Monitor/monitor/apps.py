97977dc7a16be2c0ab1afcead1816fa8b5e1d036
Paulo Vieira <paulovieira@ufg.br>
2025-08-13 23:08:28 -0300

    atualizacao versoes e app.py

--- Monitor/monitor/apps.py
+++ Monitor/monitor/apps.py
@@ -1,11 +1,15 @@
+# Monitor/monitor/apps.py
+import os
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class MonitorConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "Monitor.monitor"
-
     def ready(self):
-        # Importa e inicia o scheduler
+        # Evita executar em cada reload do runserver e em DEBUG
+        if os.environ.get("RUN_MAIN") != "true":
+            return
+        if getattr(settings, "DEBUG", True):
+            return
         from .services.scheduler import agendar_tarefas
         agendar_tarefas()
